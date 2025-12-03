4798b4aaa668a2014fcced724f4bd2e5705799a8
Shudufhadzo Tshimuka <shudufhadzo118@gmail.com>
2025-12-03 14:09:46 +0200

    Update API key in script.js

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const sendMessageButton = document.querySelector("#send-message");
 
 const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
-const API_KEY = "AIzaSyCe6e6oAlBDdGLsGILcvLfU75q2Y_6nAiY";
+const API_KEY = "AIzaSyAFKnQcqiGTlKStSfybFFVXG-0l6t_23R0";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
